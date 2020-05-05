f1a3b81fd584470585f0abb9a98ff1c9516fa12a
Jacob <jacob.wardzala@gmail.com>
2020-05-04 19:18:20 -0500

    clean heatmap

--- hBondAnalysis.py
+++ hBondAnalysis.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
-from itertools import product
+# Shows all columns of dataframe instead of the front and end only
 pd.set_option('display.max_columns', None)
 
 # variables to be changed based on input
 input_file = './data/dump.productionAce1load.lammpstrj'
-snapshots_to_read = 1
+snapshots_to_read = 501# Number of timesteps to read from traj file
 num_MOF_atoms = 3648 # Atoms in the MOF (UIO66)
 MOF_Formula = {'c': 48, 'H': 28, 'O': 32, 'Zr': 6} # Dictionary form of the molecular formula for the MOF
 loading = 1 # level of loading  (diffusing molecules per primative cell)
 atoms_per_adsorbate = 4 # number of atoms in the diffusing molecule
 
-# Number of atoms = MOFAtoms + AtomsPerAdsorbate * AdsorbateAtomsPerPrimativeCell(loading) * numPrimativeCells(MOFATOMS/ATOMSPERPRIMATIVECELL)
+# total atoms in system = MOFAtoms + AtomsPerAdsorbate * AdsorbateAtomsPerPrimativeCell(loading) * numPrimativeCells(MOFATOMS/ATOMSPERPRIMATIVECELL)
 num_atoms = int(num_MOF_atoms + loading * atoms_per_adsorbate * num_MOF_atoms/(sum(MOF_Formula.values())))
 print('Atoms in system: ', num_atoms)
 
@@ -24,11 +24,12 @@ hydrogenBondDist = 3 # Acetone-Hydrogen hydrogen bonding distance
 # Determines if  atom1s are within cutoff (bonding) distance of atom2s
 # for finding mu3 hydrogens, pass dataframe of hydrogens in as atom1 and mu3 oxygens as atom2
 # for finding acetone oxygens that are hydrogen bonding to mu3 hydrogens, pass acetone oxygens as atom1 and mu3 hydrogens (the result of the above calc) as atom2
+# Don't think the order passed in should have an affect
 def findMolPairsWithinDistance(ATOM1DF, ATOM2DF, cutoff, oneBondPerAtom1 = False):
+
         # Create a dataframe of ATOM1-ATOM2 pairings
         # Every ATOM1 paired with every ATOM2
-        #pairs = pd.DataFrame(list(product(ATOM1DF['Atom'], ATOM2DF['Atom'])), columns = ['Atom1', 'Atom2'])
-     #   print('Number of ATOM1-ATOM2 pairings (should equal number of ATOM1s * number of ATOM2s):', len(pairs))
+        # The df should be each ATOM1 paired with each ATOM2
 
         # By concatanating the coordinates of ATOM1 k times, a list that matches the number of ATOM1-ATOM2 pairings is
         # created, where k is the number of ATOM2s. This list goes through every ATOM1 atom 1 time before repeating the list again k times
@@ -36,18 +37,19 @@ def findMolPairsWithinDistance(ATOM1DF, ATOM2DF, cutoff, oneBondPerAtom1 = False
         # In contrast, the ATOM2 coordinate list is repeated n times for each ATOM1 before moving to the next atom
         # where n is the number of ATOM1s in the system
 
-        # By joining these 2 lists with the pairs of atom numbers, a dataframe is formed that has the format:
-        # ATOM1 #; ATOM2 #; ATOM1 X coord; ATOM1 Y coord; ATOM1 Z coord; ATOM2 X coord; ATOM2 Y coord; ATOM2 Z coord;
+        # By joining these 2 dataframes a dataframe is formed that has the format:
+        # ATOM1#; ATOM1 X coord; ATOM1 Y coord; ATOM1 Z coord; ATOM2#; ATOM2 X coord; ATOM2 Y coord; ATOM2 Z coord;
         # This dataframe is also joined by a Distance list, which is the calculated distance between the two atoms atoms in the given row
-        # Calculations can be checked easily by printing the Overall dataframe, which has atom#,x,y,z for the two atom types as well as the distance betweeen the two
+        # Calculations can be checked easily by printing the Overall dataframe
+        # The atom numbers match those in the trajectory file, so can be cross-refrenced there
 
         # ATOM1 coordinates repeated end on end for the number of ATOM2s there are in the system
         df1_repeated = pd.concat([ATOM1DF[['Atom','x', 'y', 'z']]]*ATOM2DF.shape[0], ignore_index=True)
         df1_repeated.columns = ['Atom1', '1x', '1y', '1z']
-        # ATOM2 repeated n times for each atom before the next atom is added to the list.
+        # ATOM2 repeated n times for each atom before the next atom is added to the list
         df2_repeated = ATOM2DF[['Atom', 'x', 'y', 'z']].loc[ATOM2DF.index.repeat(ATOM1DF.shape[0])].reset_index(drop=True)
         df2_repeated.columns = ['Atom2', '2x', '2y', '2z']
-        print(len(df1_repeated), len(df2_repeated))
+ #       print(len(df1_repeated), len(df2_repeated))
         # Store just the coordinates as arrays
         Loc1 = np.array(df1_repeated[['1x', '1y', '1z']])
         Loc2 = np.array(df2_repeated[['2x', '2y', '2z']])
@@ -56,28 +58,29 @@ def findMolPairsWithinDistance(ATOM1DF, ATOM2DF, cutoff, oneBondPerAtom1 = False
         distances = pd.DataFrame(np.sqrt((Loc1[:, 0] - Loc2[:, 0]) ** 2 + (Loc1[:, 1] - Loc2[:, 1]) ** 2 + (Loc1[:, 2] - Loc2[:, 2]) ** 2), columns = ['Distance'])
 
        # Overall data frame of ATOM1-ATOM2
-        Overall = pd.concat([df1_repeated, df2_repeated, distances], axis=1, sort=False)
-
-        print('Overall')
-        print(Overall.shape)
+        Overall = pd.concat([df1_repeated, df2_repeated, distances], axis=1).reset_index(drop=True)
+        #print('Overall')
+        #print(Overall.shape)
         #print(Overall[:5]) # Can check that distances match coordinate values and that atom numbers are coorect by comparing atom numbers in this df to atom numbers in the dump file
 
-        # if oneBondPerAtom1 then finds the minimum distance betweeen atoms for each ATOM1 and ignores all other pairings
+        # if oneBondPerAtom1 then finds the minimum distance betweeen atoms for each unique ATOM1 and ignores all other pairings
         # Then checks the remaining pairs (1 for each ATOM1) against the cutoff distance
-       # print('test')
-        #print(Overall[Overall['Distance'] < cutoff])
+        # Might be useful for 0 loading step to avoid many acetones being hydrogen bonded to 1 mu3OH
         if oneBondPerAtom1:
-        # This filters out oxygen molecules but the closest one to the hydrogen
+            # This filters out all ATOM2s but the one closest to ATOM1
             minimumDistPerMover = Overall.groupby('Atom1')['Distance'].min()
+            # Checks against the cutoff value, storing only the pairs that are within that distance
             BondsOccuring = pd.DataFrame({'Distance': minimumDistPerMover[minimumDistPerMover.values <= cutoff]})
             final = Overall[(Overall['Atom1'].isin(list(BondsOccuring.index))) & (Overall['Distance'].isin(BondsOccuring['Distance']))]
 
         # Else just check the pair distances against the cutoff distance
-        # This has the possibility for more than 1 ATOM1 to be paired with 1 ATOM2 or vice versa
+        # This has the possibility for more than 1 ATOM2 to be paired with 1 ATOM1 or vice versa
         else:
+            print("final")
+            print(Overall)
+            print(Overall.sort_values('Distance'))
             final = Overall[Overall['Distance'] < cutoff]
 
-        #print(final_muH)
         #print('Number of bonded:', final.shape[0])
         return final
 
@@ -130,19 +133,23 @@ for snp in range(snapshots_to_read):
         # Atom1 in mu3OH is oxygen, Atom2 is hydrogen; x1,y1,z1 are coords for oxygen; x2,y2,z2 are coords for hydrogen
         mu3OH = findMolPairsWithinDistance(MOFOxygen, MOFHydrogen, mu3OHBondDist, oneBondPerAtom1 = False)
         print('mu3O-H Pairs:')
-        print(mu3OH.shape)
+#        print(mu3OH.shape)
         #print(mu3OH.shape)
         # Some dataframe rearanging to format it for passing back into findMolPairsWithinDistance
         # Select only the hydrogens for acetone hydrogen bonding calculations
         mu3H = mu3OH[['Atom2', '2x', '2y', '2z']]
         # Columns need to be 'Atom', 'x', 'y', 'z'
         mu3H.columns = ['Atom', 'x', 'y', 'z']
+
+        # show dataframe of just mu3 hydrogen
         print('mu3Hs:')
         print(mu3H[:5])
 
         # Finds mu3H-AcetoneO pairs that are within the hydrogen bonding cutoff distance
         # Atom1 in is acetone's oxygen, Atom2 is mu3 Hydrogen; x1,y1,z1 are coords for the oxygen; x2,y2,z2 are coords for hydrogen
         aceOmu3H = findMolPairsWithinDistance(Ace_Oxygen, mu3H, hydrogenBondDist, oneBondPerAtom1 = False)
+
+        # Show pairs of mu3 Hydrogen and acetone oxygen that are in hydrogen bonding distance
         print('mu3H - Acetone O Hydrogen Bond Pairs:')
         print(aceOmu3H)
 
@@ -152,7 +159,7 @@ for snp in range(snapshots_to_read):
         totalAceCount += len(Ace_Oxygen)
 
 
-#print('Values across {0} snapshots:'.format(snapshots_to_read))
-#print('Number of hydrogen bonding acetone molecules:', Ace_mu3HCount)
-#print('Total number of acetone molecules:',totalAceCount)
-#print('Fraction of acetone molecules in hydrogen bonds with mu3OHs:', Ace_mu3HCount/totalAceCount)
+print('Values across {0} snapshots:'.format(snapshots_to_read))
+print('Number of hydrogen bonding acetone molecules:', Ace_mu3HCount)
+print('Total number of acetone molecules:',totalAceCount)
+print('Fraction of acetone molecules in hydrogen bonds with mu3OHs:', float(Ace_mu3HCount)/float(totalAceCount))
